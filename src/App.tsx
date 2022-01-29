afae8c7100269735dcc27b79acdcc60d2a759b67
Aaron Marsh <aaronmarsh755@gmail.com>
2022-01-28 21:59:03 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages"
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/theme/index.ts
@@ -0,0 +1,2 @@
+export const blue = "#00274C";
+export const yellow = "#FFCB05"
